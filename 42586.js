0309045cb735083b395f8f8dd73551ba526e7621
chaeyeon <71383753+yeon0914@users.noreply.github.com>
2021-10-26 16:49:56 +0900

    Create 42586.js

---
+++ 42586.js
@@ -0,0 +1,24 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    
+    var day = [];
+    var daycount=0;
+    var pcount=0;
+    for(var i=0; i<progresses.length; i++){
+        day.push(Math.ceil((100-progresses[i])/speeds[i]))
+        if (i==0)
+            daycount=day[i];
+        if(day[i]<=daycount){
+            pcount=pcount+1;
+        }
+        else{
+            answer.push(pcount);
+            pcount=1;
+            daycount=day[i];
+        }
+    }
+    answer.push(pcount);
+       
+    
+    return answer;
+}
