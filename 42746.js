dcb21efc0dc1da4f51f0881bc33576e59481fb3a
chaeyeon <yeonoey0914@gmail.com>
2021-11-05 11:22:58 +0900

    코딩테스트 연습 > 정렬 > 가장 큰 수

---
+++ 42746.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+    
+    numbers.sort(function(a, b){
+        var x = a.toString();
+        var y = b.toString();
+        var i;
+        if(x+y>y+x)
+            return -1;
+        else if (y+x>x+y)
+            return 1;
+        return 0;
+    });
+    var answer="";
+    for(var i=0; i<numbers.length; i++){
+        answer=answer+numbers[i];
+    }
+    if(answer[0]=='0')
+        return "0";
+    return answer;
+}
