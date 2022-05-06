d62e0964a0dba3d85d6b3af166a64b3f89500e69
chaeyeon <yeonoey0914@gmail.com>
2022-05-07 01:45:04 +0900

    코딩테스트 연습 < 2022 KAKAO BLIND ... < k진수에서 소수 개수 구하기

---
+++ 92335.js
@@ -0,0 +1,35 @@
+function solution(n, k) {
+    var num="";
+    while(1){
+        var a = Math.floor(n/k);
+        if(a===0){
+            num=n+num;
+            break;
+        }
+        var b = n-a*k;
+        n=a;        
+        num=b+num;
+    }
+    
+    var prime = num.split('0');
+    var answer = 0;
+    for(var i=0; i<prime.length; i++){
+        if(prime[i]===''||prime[i]==='1') continue;
+        if(isPrime(parseFloat(prime[i])))
+            answer++;
+    }
+
+    return answer;
+}
+
+function isPrime(num) {
+  if(num === 2)
+  return true;
+
+  for(let i = 2; i<=Math.sqrt(num); i++){
+    if(num % i === 0){
+      return false;
+    }
+  }
+  return true;
+}
